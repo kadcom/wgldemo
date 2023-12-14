f791f9b0dd3988b5b0cd4afe56dc82f7687aef41
Didiet Noor <lynxluna@gmail.com>
2023-12-14 23:07:05 +0700

    Triangle within NDC

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,32 @@
 import vert from './shader.vert';
 import frag from './shader.frag';
 
-const loadShader = (gl: WebGL2RenderingContext, type: number, source: string): WebGLShader | null => {
+const initBuffers = (gl: WebGL2RenderingContext): WebGLBuffer|null => {
+  const vertices = new Float32Array([
+    0.0, 0.5,
+    -0.5, -0.5,
+    0.5, -0.5
+  ]);
+
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  return buffer;
+}
+
+const drawTriangle = (gl: WebGL2RenderingContext, program: WebGLProgram, buffer: WebGLBuffer) => {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+  const positionLocation = gl.getAttribLocation(program, 'aVertexPosition');
+  gl.enableVertexAttribArray(positionLocation);
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+const loadShader = (gl: WebGL2RenderingContext, type: number, source: string): WebGLShader|null => {
   const sh = gl.createShader(type);
 
   if (sh == null) {
@@ -29,6 +54,7 @@ const initShaderProgram = (gl: WebGL2RenderingContext): WebGLProgram|null => {
   if (prog == null) {
     return null
   }
+
   gl.attachShader(prog, vs);
   gl.attachShader(prog, fs);
   gl.linkProgram(prog);
@@ -54,18 +80,30 @@ const initWebGL = (canvasId: string): WebGL2RenderingContext | null => {
   return gl
 }
 
-const render = (gl: WebGL2RenderingContext) => {
+const render = (gl: WebGL2RenderingContext, program: WebGLProgram, buf: WebGLBuffer) => {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  requestAnimationFrame(()  => render(gl))
+  drawTriangle(gl, program, buf);
+
+  requestAnimationFrame(()  => render(gl, program, buf))
 }
 
 const main = () => {
   const gl = initWebGL('webgl-canvas');
   if (!gl) return;
 
-  render(gl);
+  const program = initShaderProgram(gl);
+
+  if (!program) return;
+
+  const buffer = initBuffers(gl);
+
+  if (!buffer) return;
+
+
+
+  render(gl, program, buffer);
 }
 
 window.onload = main
