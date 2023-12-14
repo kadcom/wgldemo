8dd9c9660cc8d7d504e3b9b6a3881503e2a4d2f2
Didiet Noor <lynxluna@gmail.com>
2023-12-14 10:12:03 +0700

    Add shader and fix buisd source

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,57 @@
 /// <reference lib="dom" />
 
+import vert from './shader.vert';
+import frag from './shader.frag';
+
+const loadShader = (gl: WebGL2RenderingContext, type: number, source: string) : WebGLShader | null => {
+  const sh = gl.createShader(type);
+
+  if (sh == null) {
+    return null
+  }
+
+  gl.shaderSource(sh, source);
+  gl.compileShader(sh);
+
+  return sh;
+}
+
+const initShaderProgram = (gl: WebGL2RenderingContext): WebGLProgram|null => {
+  const vs = loadShader(gl, gl.VERTEX_SHADER, vert);
+  const fs = loadShader(gl, gl.FRAGMENT_SHADER, frag);
+
+  if ((fs == null) || (vs == null)) {
+    return null;
+
+  }
+
+  const prog = gl.createProgram();
+  if (prog == null) {
+    return null
+  }
+  gl.attachShader(prog, vs);
+  gl.attachShader(prog, fs);
+  gl.linkProgram(prog);
+
+  if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+    console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(prog));
+        return null;
+  }
+
+  return prog;
+}
+
 const initWebGL = (canvasId: string): WebGL2RenderingContext | null => {
   const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
   const gl = canvas.getContext('webgl2');
+
 
   if (!gl) {
     alert("Unable to initialise WebGL 2, Your browser may not support it");
     return null 
   }
+
+  initShaderProgram(gl);
 
   return gl
 }
